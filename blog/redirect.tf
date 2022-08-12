ab51f7a112a1284cfeee126e454494e759564398
Alex Recker <alex@reckerfamily.com>
2022-08-12 11:07:41 -0500

    Fix

--- blog/redirect.tf
+++ blog/redirect.tf
@@ -1,8 +1,7 @@
-# Add a page rule to the domain
 resource "cloudflare_page_rule" "foobar" {
   zone_id  = cloudflare_zone.zone.id
   target   = "alexrecker.com/*"
-  priority = 1
+  priority = 100
 
   actions {
     always_use_https = false
@@ -10,6 +9,20 @@ resource "cloudflare_page_rule" "foobar" {
     forwarding_url {
       url         = "https://www.alexrecker.com/$1"
       status_code = "301"
+    }
+  }
+}
+
+resource "cloudflare_page_rule" "minify" {
+  zone_id  = cloudflare_zone.zone.id
+  target   = "https://www.alexrecker.com/*"
+  priority = 1
+
+  actions {
+    minify {
+      html = "on"
+      css  = "on"
+      js   = "on"
     }
   }
 }
